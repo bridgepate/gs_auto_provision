87e3add4e430d6763ae34fa192736852daa50c86
Brijesh Patel <bridgepatel@yahoo.com>
2015-11-02 13:50:35 +1300

    Added a script which takes .txt file as input and gives .yml file. Added two .txt config file for our phone models

---
+++ writeconf_yaml.rb
@@ -0,0 +1,30 @@
+#This script will take a config file as an input and gives a yaml file as output. Argument 1 is input file and 2 is output file
+#!/usr/bin/ruby
+in_file = ARGV[0]
+out_file = ARGV[1]
+open("#{out_file}","w") do | f|
+  f.puts "---"
+  f_read = File.open("#{in_file}","r")
+  arr = ["P270" ,"P271","P47","P35","P34","P3","P33","P337", "P352","P417", "P401","P402","P404","P406","P407", "P426","P517","P501", "P502","P504","P506", "P507","P526", "P617","P601","P602","P604","P606","P607", "P626"]
+  f_read.each do | l|
+    if  (l.match(/^#/)) || (l == "\n")
+      next
+    else
+      attr = l.split("=")[0]
+      val = l.split("=")[1].to_s
+      val = val.chomp
+      special = "?<>',?[]}{=-)(*&^%$#`~{}@"
+      regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+      if val =~ regex
+        val = "\"#{val}\""
+      end
+       if arr.include?(attr)
+         puts "found"
+       else
+        f.puts "#{attr}: #{val}"
+      end
+    end
+
+  end
+
+end
